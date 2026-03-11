93d653274bab6a4079a7226e834239a6ed5d86b3
Mallikarjuna84 <mallikarjunap8426@gmail.com>
2026-03-12 00:52:19 +0530

    frontend

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+// Import Pages
+import HomePage from './pages/HomePage';
+import ProductListPage from './pages/ProductListPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import CartPage from './pages/CartPage';
+import OrderHistoryPage from './pages/OrderHistoryPage';
+
+// Import Auth Selector
+import { selectIsAuthenticated } from './store/authSlice';
+
+// Protected Route
+function ProtectedRoute({ element }) {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  return isAuthenticated ? element : <Navigate to="/login" replace />;
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/cart" element={<CartPage />} />
+
+          {/* Protected Route */}
+          <Route
+            path="/order-history"
+            element={<ProtectedRoute element={<OrderHistoryPage />} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
